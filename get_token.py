5b0bd177b7de962dd41a0ce1b3a1e19d66a0a03d
IvanDurnev <durnev87@gmail.com>
2018-12-24 20:47:45 +0900

    version 1

---
+++ get_token.py
@@ -0,0 +1,12 @@
+from urllib.parse import urlencode, urlsplit
+
+APP_ID = 6773685
+AUTH_URL = 'https://oauth.vk.com/authorize?'
+auth_params = {
+    'client_id': APP_ID,
+    'redirect_uri': 'https://oauth.vk.com/blank.html',
+    'scope': 'friends, email',
+    'response_type': 'token',
+    'v': '5.92'
+}
+print(AUTH_URL+urlencode(auth_params))
